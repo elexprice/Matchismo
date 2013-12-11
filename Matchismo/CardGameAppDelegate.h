263856adabb4ef207ab3b5dc33615c6565408f02
Elex Price <elexprice@elex-prices-macbook-pro.local>
2013-12-10 20:07:07 -0600

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Elex Price on 12/10/13.
+//  Copyright (c) 2013 IE Media Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Elex Price on 12/10/13.
+//  Copyright (c) 2013 IE Media Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
